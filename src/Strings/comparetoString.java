28e5a2a33d370056cb37e36c87d14de8bd00e56d
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-09 14:20:39 +0530

    code

---
+++ src/Strings/comparetoString.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class comparetoString {
+    public static void main(String[] args) {
+        String a="keshav";
+        String b="manish";
+        System.out.println(a.compareTo(b));
+    }
+}
