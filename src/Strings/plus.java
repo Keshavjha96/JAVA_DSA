e27e0b8fd71231a1bdfaea5540ed2383237b6fb8
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-09 15:12:01 +0530

    code

---
+++ src/Strings/plus.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class plus {
+    public static void main(String[] args) {
+        String a="bhakti ";
+        String b="hi skti hai";
+        a+=b;
+        a+=10;
+        a+='\n';
+        a+='n';
+        System.out.println(a);
+    }
+}
