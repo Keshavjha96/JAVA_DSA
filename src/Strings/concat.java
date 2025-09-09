38443f8462c4153d33ef8366719f548490c57164
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-09 15:02:59 +0530

    code

---
+++ src/Strings/concat.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class concat {
+    public static void main(String[] args) {
+        String a="keshav";
+        String b="Janki";
+        a=a.concat(b);
+        System.out.println(a);
+    }
+}
