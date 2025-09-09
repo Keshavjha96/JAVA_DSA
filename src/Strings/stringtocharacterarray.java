7159e39d4435ec3e4762c92b45f6c373aa0d4c95
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-09 18:37:22 +0530

    code

---
+++ src/Strings/stringtocharacterarray.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class stringtocharacterarray {
+    public static void main(String[] args) {
+        String s="keshav";
+        char[] arr=s.toCharArray();
+        for(char ch:arr){
+            System.out.println(ch);
+        }
+    }
+}
