e018277c7672d7de8a24a77cc59ebdf77721a87b
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-11 21:36:43 +0530

    code

--- src/Strings/ReverseSb.java
+++ src/Strings/ReverseSb.java
@@ -4,16 +4,22 @@ public class ReverseSb {
     public static void main(String[] args) {
         String s="Keshav";
         StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        s=sb.toString();
+        System.out.println(s);
         //sb.reverse();
-        int i=0,j=sb.length()-1;
-        while(i<=j){
-            char temp1=sb.charAt(i);
-            char temp2=sb.charAt(j);
-            sb.setCharAt(i,temp2);
-            sb.setCharAt(j,temp1);
-            i++;
-            j--;
-        }
-        System.out.println(sb);
+        sb.deleteCharAt(0);
+        sb.insert(1,'o');
+        sb.delete(4,6);
+//        int i=0,j=sb.length()-1;
+//        while(i<=j){
+//            char temp1=sb.charAt(i);
+//            char temp2=sb.charAt(j);
+//            sb.setCharAt(i,temp2);
+//            sb.setCharAt(j,temp1);
+//            i++;
+//            j--;
+//        }
+      System.out.println(sb);
     }
 }
---
+++ src/TWO_D_Array/printspirall.java
@@ -0,0 +1,65 @@
+package TWO_D_Array;
+
+import java.util.Scanner;
+
+public class printspirall {
+    public static void printMatrix(int[][] matrix){
+        for (int i=0;i< matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void printspiralorder(int[][] matrix,int r,int c){
+        int topRow=0,bottomRow=r-1,leftCol=0,rightCol=c-1;
+        int totalelements=0;
+        while (totalelements<r*c){
+            //top row->leftcol to right col
+            for (int j=leftCol;j<=rightCol && totalelements<r*c;j++){
+                System.out.print(matrix[topRow][j]+" ");
+                totalelements++;
+            }
+            topRow++;
+            //right column->top row to bottom row
+            for (int i=topRow;i<=bottomRow && totalelements<r*c;i++){
+                System.out.print(matrix[i][rightCol]+" ");
+                totalelements++;
+            }
+            rightCol--;
+            //bottom row->right colum to left column
+            for (int j=rightCol;j>=leftCol && totalelements<r*c;j--){
+                System.out.print(matrix[bottomRow][j]+" ");
+                totalelements++;
+            }
+            bottomRow--;
+            //left column->bottom row to top row
+            for (int i=bottomRow;i>=topRow && totalelements<r*c;i--){
+                System.out.print(matrix[i][leftCol]+" ");
+                totalelements++;
+            }
+            leftCol++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of Rows:");
+        int r = sc.nextInt();
+        System.out.println("Enter no of Columns:");
+        int c = sc.nextInt();
+        int[][] matrix= new int[r][c];
+        System.out.println("Enter " + r * c + " Elements");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Input Matrix:");
+        printMatrix(matrix);
+        System.out.println("Spiral Order:");
+        printspiralorder(matrix,r,c);
+
+    }
+}
