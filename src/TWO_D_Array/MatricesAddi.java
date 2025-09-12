d518b12aa012490b121a4e046625b1d3f09fb52d
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-12 20:47:06 +0530

    code

---
+++ src/TWO_D_Array/MatricesAddi.java
@@ -0,0 +1,70 @@
+package TWO_D_Array;
+import java.util.Scanner;
+
+public class MatricesAddi {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " "); // Changed to print instead of println
+            }
+            System.out.println(); // For new line after each row
+        }
+    }
+
+    public static void add(int[][] a, int r1, int c1, int[][] b, int r2, int c2) {
+        if (r1 != r2 || c1 != c2) {
+            System.out.println("Wrong Input - Addition not possible");
+            return;
+        }
+
+        int[][] sum = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("Sum of Matrix 1 and Matrix 2:");
+        printMatrix(sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input for Matrix 1
+        System.out.println("Enter number of Rows and Columns of Matrix 1:");
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] a = new int[r1][c1];
+        System.out.println("Enter elements of Matrix 1:");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input for Matrix 2
+        System.out.println("Enter number of Rows and Columns of Matrix 2:");
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int[][] b = new int[r2][c2];
+        System.out.println("Enter elements of Matrix 2:");
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Print both matrices
+        System.out.println("Matrix 1:");
+        printMatrix(a);
+        System.out.println("Matrix 2:");
+        printMatrix(b);
+
+        // Perform addition
+        add(a, r1, c1, b, r2, c2);
+
+        sc.close();
+    }
+}
