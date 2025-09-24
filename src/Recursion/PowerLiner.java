dd428c9ebaa4dae995d79a73b9f2b77c992442b3
Keshavjha96 <keshav.jha9625@gmail.com>
2025-09-24 22:31:55 +0530

    code

--- src/Recursion/PowerLiner.java
+++ src/Recursion/PowerLiner.java
@@ -9,6 +9,15 @@ public class PowerLiner {
         int a=sc.nextInt();
         System.out.println("Enter Exponent: ");
         int b=sc.nextInt();
-        System.out.println(a+" raised to the power "+b+" is "+Math.pow(a,b));
+        System.out.println(a+" raised to the power "+b+" is "+pow(a,b));
     }
+    public static int pow(int a,int b){
+        if(b==0) return 1;
+        if(b%2==0) return pow(a,b/2)*pow(a,b/2);
+        else return a*pow(a,b/2)*pow(a,b/2);
+    }
+//    public static int pow(int a,int b){
+//        if (b==0) return 1;
+//        return a*pow(a,b-1);
+//    }
 }
