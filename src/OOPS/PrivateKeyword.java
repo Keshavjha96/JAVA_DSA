4a8514a555e9fccd50d55de013b757cd6767177a
Keshavjha96 <keshav.jha9625@gmail.com>
2025-10-29 22:51:29 +0530

    code

--- src/OOPS/PrivateKeyword.java
+++ src/OOPS/PrivateKeyword.java
@@ -1,4 +1,39 @@
 package OOPS;
+class Students{
+    String name;//null
+   private int rno;//0
+    double cgpa;//0.0
 
+    void print(){
+        System.out.println(name+" "+cgpa+" "+rno);
+    }
+    int getRno(){    //getter
+        return rno;
+    }
+    void setRno(int x){
+        rno=x;
+    }
+//    private void print(){
+//        System.out.println(name+" "+cgpa+" "+rno);
+//    }
+//    public void p(){
+//    print();
+//    }
+}
 public class PrivateKeyword {
+
+    public static void main(String[] args) {
+        Students s1=new Students();
+        s1.print();
+       // s1.rno=12;
+        s1.cgpa=8.9;
+        s1.name="Manish";
+        s1.print();
+        s1.setRno(45);
+        System.out.println(s1.getRno());
+
+        Students s2=new Students();
+        s2.name="Keshav jha";
+        s2.print();
+    }
 }
