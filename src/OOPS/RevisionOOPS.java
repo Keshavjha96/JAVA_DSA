e50255ebf8d90540fd0bafa8eb676ab0f6effb39
Keshavjha96 <keshav.jha9625@gmail.com>
2025-10-31 13:39:03 +0530

    code

--- src/OOPS/RevisionOOPS.java
+++ src/OOPS/RevisionOOPS.java
@@ -1,27 +1,13 @@
 package OOPS;
 
 public class RevisionOOPS {
-    private static class Pokemon{
-       private int power;
-        String type;
-        String name;
-        int getPower(){
-            return power;
-        }
-        Pokemon(String type,int power){ //setter
-            this.power=power;
-            this.type=type;
-        }
-        void print(){
-            System.out.println(this.power+" "+this.type);
-        }
-    }
+
     public static void main(String[] args) {
        Pokemon pikachu=new Pokemon("Electric",70);
        pikachu.power=80;
        Pokemon jigglypuff=new Pokemon("Fairy",50);
        pikachu.print();
        jigglypuff.print();
-        System.out.println(pikachu.getPower());
+
     }
 }
