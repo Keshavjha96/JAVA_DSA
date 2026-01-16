e24a370f32c1a053d3ac213635e3c20bcd77ca77
Keshavjha96 <keshav.jha9625@gmail.com>
2026-01-16 23:22:00 +0530

    code

--- src/ArraysRevesion/takinginput.java
+++ src/ArraysRevesion/takinginput.java
@@ -1,4 +1,22 @@
 package ArraysRevesion;
-
+import java.util.Scanner;
 public class takinginput {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size:");
+            int n=sc.nextInt();
+            int [] arr=new int[n];
+
+        System.out.println("Enter array Elements:");
+            //input
+            for ( int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+
+            //output
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
 }
