4a3936557d7fe6eb274a03ba8932a2befd0608d8
Keshavjha96 <keshav.jha9625@gmail.com>
2026-02-21 17:51:57 +0530

    code

--- src/Stacks/BalancedBrackets.java
+++ src/Stacks/BalancedBrackets.java
@@ -3,27 +3,27 @@ package Stacks;
 import java.util.Stack;
 
 public class BalancedBrackets {
-    public static void main(String[] args) {
 
-    }
+
     public boolean isBalanced(String s) {
-        Stack<Character> st=new Stack<>();
-        for (int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || ch== '[' || ch=='{') st.push(ch);
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') st.push(ch);
             else {//closing bracket
-                if(st.size()==0) return false;
-                char top=st.peek();
-                if(sameStyle(top,ch)) st.pop();
+                if (st.size() == 0) return false;
+                char top = st.peek();
+                if (sameStyle(top, ch)) st.pop();
                 else return false;
+            }
         }
+        return (st.size() == 0);
     }
-        return (st.size()==0);
-}
 
     static boolean sameStyle(char a, char b) {
-       if(a=='(' && b==')') return true;
-        if(a=='[' && b==']') return true;
-        if(a=='{' && b=='}') return true;
+        if (a == '(' && b == ')') return true;
+        if (a == '[' && b == ']') return true;
+        if (a == '{' && b == '}') return true;
         return false;
     }
+}
